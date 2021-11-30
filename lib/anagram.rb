a4244b20938fab153e6189b5ab78e418da827a05
jacobloris <jacobloris@outlook.com>
2021-11-29 19:23:01 -0700

    commit tree edf879288e7e6f8cf2637afe428333ab98bc19f1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :array
+    def initialize(name)
+        @array = name
+    end
+
+    def match(word_array)
+        word_array.select do |array|
+            array.split("").sort == @array.split("").sort
+        end
+    end
+end
